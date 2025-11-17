4ff3fa8230e9d3f11144707792c33276e40f6a97
Elia Ferrari <elia.ferrari@fhnw.ch>
2025-11-17 18:10:21 +0100

    reading merged_data from main folder and not temp

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -565,16 +565,18 @@ def check_new_data(actual_db_path, new_data_path, match_columns,
     """
     old_db = pd.read_pickle(actual_db_path)
     old_db = old_db.fillna("nan")
-    old_db = old_db.replace(to_replace='', value='nan', regex=True)
+    old_db = old_db.replace(to_replace='None', value="nan")
+    # old_db = old_db.replace(to_replace='', value='nan', regex=True)
     new_db = pd.read_csv(new_data_path, low_memory=False)
     new_db = new_db.drop_duplicates(keep='first') # check duplicates
     new_db = new_db.fillna("nan")
-    new_db = new_db.replace(to_replace="'", value="nan", regex=True)
-    new_db = new_db.replace(to_replace='\"', value="nan", regex=True)
-    new_db = new_db.replace(to_replace="", value = "nan", regex=True)
-    new_db = new_db.replace(to_replace=" ", value = "nan", regex=True)
-    new_db = new_db.replace(to_replace="  ", value = "nan", regex=True)
-    new_db = new_db.replace(to_replace="    ", value = "nan", regex=True)
+    new_db = new_db.replace(to_replace='None', value="nan")
+    # new_db = new_db.replace(to_replace="'", value="nan", regex=True)
+    # new_db = new_db.replace(to_replace='\"', value="nan", regex=True)
+    # new_db = new_db.replace(to_replace="", value = "nan", regex=True)
+    # new_db = new_db.replace(to_replace=" ", value = "nan", regex=True)
+    # new_db = new_db.replace(to_replace="  ", value = "nan", regex=True)
+    # new_db = new_db.replace(to_replace="    ", value = "nan", regex=True)
     
     to_preprocessing = new_db.merge(old_db, on=match_columns, how='left',
                                     indicator='_lmerge', suffixes=(None, "_drop"))
@@ -754,9 +756,10 @@ if __name__ == "__main__":
     # Copy to artifact folder
     shutil.copyfile(config.GEOSERVICES_CH_CSV, os.path.join(config.WORKFLOW_ARTIFACT_FOLDER,'geoservices_CH.csv'))
 
-    data_to_keep = check_new_data(os.path.join(config.TEMP_PROCESSED_DATA_PKL),
+    #TODO: add column "contact" again after the  manual processing
+    data_to_keep = check_new_data(os.path.join(config.PROCESSED_DATA_PKL),
                    config.GEOSERVICES_CH_CSV,
-                   match_columns=['name','title','provider','keywords','abstract','endpoint', 'contact'],
+                   match_columns=['name','title','provider','keywords','abstract','endpoint'],
                    output_path=os.path.split(config.GEOSERVICES_CH_CSV)[0])
     
     logger.info(f"Keeping {len(data_to_keep)} datasets from old database")
