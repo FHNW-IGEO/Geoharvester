eba254e7b9443fe9ff3b24a193543cb28e79a1bf
Friedrich Striewski <friedrich.striewski@fhnw.ch>
2026-01-06 14:40:40 +0100

    paths

--- scraper/workflow/compare_hashes.py
+++ scraper/workflow/compare_hashes.py
@@ -4,9 +4,9 @@ import json
 from pathlib import Path
 
 # PREFLIGHT_HASHES = Path("preflight-hashes.jsonl") # From the preflight #TODO
-PREFLIGHT_HASHES = Path("temp_preflight-hashes.json") # From existing data
+PREFLIGHT_HASHES = Path("./data/temp_preflight-hashes.json") # From existing data
 # PROCESSED_HASHES = Path("processed-hashes.json") # From existing data #TODO - Read from repo!
-PROCESSED_HASHES = Path("data/temp_processed-hashes.json") # From existing data
+PROCESSED_HASHES = Path("./data/temp_processed-hashes.json") # From existing data
 OUTPUT = Path("datasets_to_process.json")
 
 
