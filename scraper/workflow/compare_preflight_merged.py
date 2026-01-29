3191671596e7f9edad705aa102bf7485184c11ef
Friedrich Striewski <friedrich.striewski@fhnw.ch>
2026-01-29 11:19:34 +0100

    Check file paths

--- scraper/workflow/compare_preflight_merged.py
+++ scraper/workflow/compare_preflight_merged.py
@@ -3,37 +3,37 @@
 import json
 from pathlib import Path
 
-PREFLIGHT_HASHES = Path("./temp_preflight-diagnostics.jsonl") # From preflight
-PROCESSED_HASHES = Path("./temp_processed-hashes.json") # From existing data
+PREFLIGHT_HASHES = Path("./artifacts/preflight-diagnostics.jsonl") # From preflight
+MERGED_DATA_PKL = Path("../data/merged_data.pkl") # From Git
 OUTPUT = Path("datasets_to_process.json")
 
 def main():
     if not PREFLIGHT_HASHES.exists():
         raise FileNotFoundError("preflight-hashes.json not found")
 
-    if not PROCESSED_HASHES.exists():
+    if not MERGED_DATA_PKL.exists():
         raise FileNotFoundError("processed-hashes.json not found")
 
     # Load full JSON objects (NOT jsonl)
-    preflight = json.loads(PREFLIGHT_HASHES.read_text(encoding="utf-8"))
-    processed = json.loads(PROCESSED_HASHES.read_text(encoding="utf-8"))
+    # preflight = json.loads(PREFLIGHT_HASHES.read_text(encoding="utf-8"))
+    # processed = json.loads(PROCESSED_HASHES.read_text(encoding="utf-8"))
 
-    to_process = {}
+    # to_process = {}
 
-    for dataset_id, hash_new in preflight.items():
-        hash_old = processed.get(dataset_id)
+    # for dataset_id, hash_new in preflight.items():
+    #     hash_old = processed.get(dataset_id)
 
-        if hash_old is None:
-            to_process[dataset_id] = "new"
-        elif hash_old != hash_new:
-            to_process[dataset_id] = "updated"
+    #     if hash_old is None:
+    #         to_process[dataset_id] = "new"
+    #     elif hash_old != hash_new:
+    #         to_process[dataset_id] = "updated"
 
-    OUTPUT.write_text(
-        json.dumps(to_process, indent=2, ensure_ascii=False),
-        encoding="utf-8",
-    )
+    # OUTPUT.write_text(
+    #     json.dumps(to_process, indent=2, ensure_ascii=False),
+    #     encoding="utf-8",
+    # )
 
-    print(f"Datasets to process: {len(to_process)}")
+    # print(f"Datasets to process: {len(to_process)}")
 
 
 if __name__ == "__main__":
