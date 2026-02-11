ac0882ac978256f3f55bc7e3dd1abf00b3661b09
Friedrich Striewski <friedrich.striewski@fhnw.ch>
2026-02-11 13:23:53 +0100

    Cleanup script and cleaned data

--- scraper/data/clean_pickle.py
+++ scraper/data/clean_pickle.py
@@ -4,21 +4,13 @@ from datetime import datetime
 df = pd.read_pickle("merged_data.pkl")
 
 # inspect rows
-# print(df[df["abstract"] == "webgis@swisstopo.ch"])
-# count_faulty = len(df[df["abstract"] == "webgis@swisstopo.ch"])
-# print("faulty: ", count_faulty)
+print(df[df["abstract"] == "webgis@swisstopo.ch"])
+count_faulty = len(df[df["abstract"] == "webgis@swisstopo.ch"])
+print("faulty: ", count_faulty)
 
 # drop rows by condition
-#df = df[df["abstract"] != "webgis@swisstopo.ch"] # keeps everything that is not (exclude)
+# df = df[df["abstract"] != "webgis@swisstopo.ch"] # keeps everything that is not (exclude)
 
 # save back
-#df.to_pickle("cleaned_merged_data.pkl")
+#df.to_pickle("merged_data.pkl")
 
-
-# -------------------
-# Manually add timestamp and reason
-# df["reason"] = "existing"
-# timestamp = datetime.now().strftime("%d, %m, %Y")
-# df["timestamp"] = timestamp
-# # save back
-# df.to_pickle("cleaned_merged_data.pkl")
