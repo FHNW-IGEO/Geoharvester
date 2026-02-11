f1725b436c55d7fbfb95aceee25b1081de90f31f
Friedrich Striewski <friedrich.striewski@fhnw.ch>
2026-02-11 15:08:06 +0100

    Fix route to BE

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,12 @@ import checker from "vite-plugin-checker";
 export default defineConfig({
   plugins: [react(), checker({ typescript: true })],
   server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+      },
+    },
     port: 3000,
   },
   resolve: {
