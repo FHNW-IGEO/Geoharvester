14895684b1a5ceae49c50966b767fdcf0b88f157
Friedrich Striewski <friedrich.striewski@fhnw.ch>
2026-03-03 15:17:42 +0100

    Subrow width

--- client/src/components/table/SubRow.tsx
+++ client/src/components/table/SubRow.tsx
@@ -24,6 +24,7 @@ import {
 } from "../../requests";
 import { Geoservice, SearchParameters } from "../../types";
 import { PROVIDER } from "../../appConstants";
+import { alpha, Box } from "@mui/system";
 
 export const SubRow = ({
   row,
@@ -78,57 +79,66 @@ export const SubRow = ({
   }));
 
   return (
-    <StyledTableRow key={index}>
+    <StyledTableRow
+      key={index}
+      sx={{
+        visibility: open ? "visible" : "collapse",
+      }}
+    >
       <TableCell
         style={{
           textAlign: "left",
           padding: 0,
-          boxShadow: "inset 0px 0px 6px 0px rgba(0, 0, 0, 0.15)",
+          backgroundColor: alpha(theme.palette.primary.main, 0.04),
         }}
-        colSpan={mobileMode ? 5 : 6}
+        colSpan={2}
       >
         <Collapse in={open} timeout="auto" unmountOnExit>
-          <Stack direction="row" spacing={1} sx={{ width: "100%" }}>
-            <Stack id="subRowField1">
-              <Typography variant="button" sx={{ paddingBottom: 1 }}>
-                {intl.formatMessage({
-                  id: "subrow.details",
-                  defaultMessage: "Details: ",
-                })}
-              </Typography>
-              <Typography variant="body2" sx={{ paddingBottom: 2 }}>
-                {intl.formatMessage({
-                  id: "subrow.contact",
-                  defaultMessage: "Kontakt: ",
-                })}
-                {row.contact}
-              </Typography>
-
+          <Box sx={{ p: 1 }}>
+            <Typography variant="button" sx={{ paddingBottom: 1 }}>
+              {intl.formatMessage({
+                id: "subrow.details",
+                defaultMessage: "Details: ",
+              })}
+            </Typography>
+            <Typography variant="body2" sx={{ paddingBottom: 2 }}>
+              {intl.formatMessage({
+                id: "subrow.contact",
+                defaultMessage: "Kontakt: ",
+              })}
+              {row.contact}
+            </Typography>
+            <Stack>
               <Typography variant="caption">
                 <span className="bold">Name:</span>
                 <span className="long-url">{row.name}</span>
               </Typography>
-              <Stack>
-                <Typography variant="caption">
-                  <span className="bold">Endpoint:</span>
-                  <span className="long-url"> {row.endpoint}</span>
-                </Typography>
-                <Typography variant="caption">
-                  <span className="bold">Tree:</span>
-                  <span className="long-url">{row.tree}</span>
-                </Typography>
-                <Typography variant="caption">
-                  <span className="bold">Group:</span>
-                  <span className="long-url">{row.group}</span>
-                </Typography>
-              </Stack>
+              <Typography variant="caption">
+                <span className="bold">Endpoint:</span>
+                <span className="long-url"> {row.endpoint}</span>
+              </Typography>
+              <Typography variant="caption">
+                <span className="bold">Tree:</span>
+                <span className="long-url">{row.tree}</span>
+              </Typography>
+              <Typography variant="caption">
+                <span className="bold">Group:</span>
+                <span className="long-url">{row.group}</span>
+              </Typography>
             </Stack>
-
-            <Stack
-              sx={{
-                flexGrow: 1,
-              }}
-            >
+          </Box>
+        </Collapse>
+      </TableCell>
+      {!mobileMode && (
+        <TableCell
+          style={{
+            textAlign: "left",
+            padding: 0,
+            backgroundColor: alpha(theme.palette.primary.main, 0.04),
+          }}
+        >
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ p: 1 }}>
               <div id="subRowField2">
                 <div>
                   <Typography variant="button">
@@ -191,89 +201,107 @@ export const SubRow = ({
                   ))}
                 </div>
               </div>
-            </Stack>
-            {!mobileMode && (
-              <Stack sx={{ minWidth: 120, maxWidth: 120 }} id="subRowField3">
-                <Typography variant="button">
-                  {intl.formatMessage({
-                    id: "subrow.tools",
-                    defaultMessage: "Tools: ",
-                  })}
-                </Typography>
-
+            </Box>
+          </Collapse>
+        </TableCell>
+      )}
+      <TableCell
+        colSpan={1}
+        style={{
+          textAlign: "left",
+          padding: 0,
+          backgroundColor: alpha(theme.palette.primary.main, 0.04),
+        }}
+      ></TableCell>
+      <TableCell
+        colSpan={2}
+        style={{
+          textAlign: "left",
+          padding: 0,
+          backgroundColor: alpha(theme.palette.primary.main, 0.04),
+        }}
+      >
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          <Box sx={{ p: 1 }}>
+            <Typography variant="subtitle1">
+              {intl.formatMessage({
+                id: "subrow.tools",
+                defaultMessage: "Tools: ",
+              })}
+            </Typography>
+            <Stack spacing={1}>
+              <Tooltip
+                title={intl.formatMessage({ id: "button.preview" })}
+                arrow
+              >
+                <Button
+                  sx={{ justifyContent: "flex-start" }}
+                  fullWidth
+                  startIcon={<LaunchIcon />}
+                  variant="text"
+                  onClick={() => {
+                    const url =
+                      row.provider === PROVIDER.BUND
+                        ? row.preview.replace("??", "'")
+                        : row.preview;
+                    window.open(url);
+                  }}
+                  disabled={NULLVALUES.includes(row.preview)}
+                >
+                  MapGeo
+                </Button>
+              </Tooltip>
+              {row.legend && row.legend.includes("http") && (
                 <Tooltip
-                  title={intl.formatMessage({ id: "button.preview" })}
+                  title={intl.formatMessage({ id: "button.legend" })}
                   arrow
                 >
                   <Button
                     sx={{ justifyContent: "flex-start" }}
                     fullWidth
-                    startIcon={<LaunchIcon />}
+                    startIcon={<InsertPhotoIcon />}
                     variant="text"
                     onClick={() => {
-                      const url =
-                        row.provider === PROVIDER.BUND
-                          ? row.preview.replace("??", "'")
-                          : row.preview;
-                      window.open(url);
+                      window.open(row.legend);
                     }}
-                    disabled={NULLVALUES.includes(row.preview)}
+                    disabled={NULLVALUES.includes(row.legend)}
                   >
-                    MapGeo
+                    Legende
                   </Button>
                 </Tooltip>
-                {row.legend && row.legend.includes("http") && (
-                  <Tooltip
-                    title={intl.formatMessage({ id: "button.legend" })}
-                    arrow
-                  >
-                    <Button
-                      sx={{ justifyContent: "flex-start" }}
-                      fullWidth
-                      startIcon={<InsertPhotoIcon />}
-                      variant="text"
-                      onClick={() => {
-                        window.open(row.legend);
-                      }}
-                      disabled={NULLVALUES.includes(row.legend)}
-                    >
-                      Legende
-                    </Button>
-                  </Tooltip>
-                )}
-                <Tooltip
-                  title={intl.formatMessage({ id: "button.arcgis_handler" })}
-                  arrow
+              )}
+              <Tooltip
+                title={intl.formatMessage({ id: "button.arcgis_handler" })}
+                arrow
+              >
+                <Button
+                  sx={{ justifyContent: "flex-start" }}
+                  fullWidth
+                  variant="text"
+                  onClick={routeObjectBuilder().arcgis_handler}
+                  startIcon={<DownloadIcon />}
+                  disabled={NULLVALUES.includes(row.endpoint)}
                 >
-                  <Button
-                    sx={{ justifyContent: "flex-start" }}
-                    fullWidth
-                    variant="text"
-                    onClick={routeObjectBuilder().arcgis_handler}
-                    startIcon={<DownloadIcon />}
-                    disabled={NULLVALUES.includes(row.endpoint)}
-                  >
-                    ArcGIS
-                  </Button>
-                </Tooltip>
-                <Tooltip
-                  title={intl.formatMessage({ id: "button.qgis_handler" })}
-                  arrow
+                  ArcGIS
+                </Button>
+              </Tooltip>
+              <Tooltip
+                title={intl.formatMessage({ id: "button.qgis_handler" })}
+                arrow
+              >
+                <Button
+                  sx={{ justifyContent: "flex-start" }}
+                  fullWidth
+                  variant="text"
+                  onClick={routeObjectBuilder().qgis_handler}
+                  startIcon={<DownloadIcon />}
+                  disabled={NULLVALUES.includes(row.endpoint)}
                 >
-                  <Button
-                    sx={{ justifyContent: "flex-start" }}
-                    fullWidth
-                    variant="text"
-                    onClick={routeObjectBuilder().qgis_handler}
-                    startIcon={<DownloadIcon />}
-                    disabled={NULLVALUES.includes(row.endpoint)}
-                  >
-                    QGIS
-                  </Button>
-                </Tooltip>
-              </Stack>
-            )}
-          </Stack>
+                  QGIS
+                </Button>
+              </Tooltip>
+            </Stack>
+          </Box>
         </Collapse>
       </TableCell>
     </StyledTableRow>
